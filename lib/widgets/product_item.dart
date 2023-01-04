1ae30ac1f619a91ad71340f716f89bc7ad23cea4
victorac <victorde.ac@gmail.com>
2023-01-04 14:27:30 -0300

    add Consumer to split listening updates

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -9,17 +9,19 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
+    final product = Provider.of<Product>(context, listen: false);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         footer: GridTileBar(
           backgroundColor: Colors.black87,
-          leading: IconButton(
-            onPressed: product.toggleIsFavorite,
-            color: Theme.of(context).colorScheme.secondary,
-            icon: Icon(
-              product.isFavorite ? Icons.favorite : Icons.favorite_border,
+          leading: Consumer<Product>(
+            builder: (_, product, __) => IconButton(
+              onPressed: product.toggleIsFavorite,
+              color: Theme.of(context).colorScheme.secondary,
+              icon: Icon(
+                product.isFavorite ? Icons.favorite : Icons.favorite_border,
+              ),
             ),
           ),
           title: Text(
